8b759e7eb332ebbe002aab64d5046a883d5cae2f
kiril <kiril@DESKTOP-FMO0OTQ>
2023-08-15 18:35:17 +0300

    Add project files.

---
+++ RequestProcessingPipeline/FromElevenToNineteenMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace RequestProcessingPipeline
+{
+    public class FromElevenToNineteenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FromElevenToNineteenMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string? token = context.Request.Query["number"];
+            try
+            {
+                int number = Convert.ToInt32(token);
+                number = Math.Abs(number);
+                if (number < 11 || number > 19)
+                {
+                    await _next.Invoke(context);
+                }
+                else
+                {
+                    string[] Numbers = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                    await context.Response.WriteAsync("Your number is " + Numbers[number - 11]);
+                }
+            }
+            catch (Exception)
+            {
+                await context.Response.WriteAsync("Incorrect parameter");
+            }
+        }
+    }
+}
